18a7ebca63fa27c4ac6e39df8651795cc663d0da
Aleksandr Zagoskin <zaleksandrne@gmail.com>
2020-12-12 00:07:24 +0300

    vie

--- flowers/views.py
+++ flowers/views.py
@@ -13,7 +13,6 @@ from django.http import JsonResponse
 
 def index(request):
     flowers = Flowers.objects.all().values()
-    with open("/home/alex/code/roma/romananan/base_flowers.json", "w") as write_file:
+    with open("/home/alex/code/roma/roma/base_flowers.json", "w") as write_file:
         json.dump(list(flowers), write_file)
-    return render(request, 'index.html', {"flowers": flowers}
-     ) 
+    return render(request, 'index.html', {"flowers": flowers}) 
