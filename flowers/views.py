8f689973479ea7facbb35200507565693bab149e
Aleksandr Zagoskin <zaleksandrne@gmail.com>
2020-12-11 23:04:30 +0300

    flowers_base

--- flowers/views.py
+++ flowers/views.py
@@ -1,11 +1,19 @@
-
 from django.shortcuts import render
 from .models import Flowers
+import json
+from django.http import JsonResponse
 
+
+
+
+#queryset = YourModel.objects.filter(some__filter="some value").values()
+#return JsonResponse({"models_to_return": list(queryset)})
 
 
 
 def index(request):
-    flowers_list = Flowers.objects.all()
-    return render(request, 'index.html', {"flowers_list": flowers_list}
+    flowers = Flowers.objects.all().values()
+    with open("base_flowers.json", "w") as write_file:
+        json.dump(list(flowers), write_file)
+    return render(request, 'index.html', {"flowers": flowers}
      ) 
