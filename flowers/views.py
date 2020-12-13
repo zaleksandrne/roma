38b73f1dff1fa507e63ef48a7b5703624660a681
Aleksandr Zagoskin <zaleksandrne@gmail.com>
2020-12-13 17:02:49 +0300

    base in static

--- flowers/views.py
+++ flowers/views.py
@@ -5,10 +5,10 @@ import json
 
 def index(request):
     flowers = Flowers.objects.all().values()
-    with open("/home/alex/code/roma/roma/base_flowers.json", "w") as write_file:
+    with open("/home/alex/code/roma/roma/flowers/static/base_flowers.json", "w") as write_file:
         json.dump(list(flowers), write_file, ensure_ascii=False)
     return render(request, 'index.html', {"flowers": flowers}) 
 
 
 
-#/home/alex/code/roma/roma/base_flowers.json
+#/home/alex/code/roma/roma/flowers/static/base_flowers.json
