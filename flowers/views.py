662b300acf77c940e00a31a6466f072a193461d4
Aleksandr Zagoskin <zaleksandrne@gmail.com>
2020-12-14 23:06:46 +0300

    .

--- flowers/views.py
+++ flowers/views.py
@@ -20,10 +20,10 @@ def index(request):
 def page_not_found(request, exception):
     return render(
         request, 
-        "misc/404.html", 
+        "404.html", 
         {"path": request.path}, 
         status=404
     )
 
 def server_error(request):
-    return render(request, "misc/500.html", status=500) 
+    return render(request, "500.html", status=500) 
--- roma/urls.py
+++ roma/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import handler404, handler500
 
-handler404 = "posts.views.page_not_found"
-handler500 = "posts.views.server_error" 
+handler404 = "flowers.views.page_not_found"
+handler500 = "flowers.views.server_error" 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
